70057bd8da1a026522ac427bba6d23cd9ba3bdfc
Mateusz Róg <kontakt@matrog.pl>
2024-01-11 21:08:11 +0100

    Created firm form

--- app/controllers/workoutController.ts
+++ app/controllers/workoutController.ts
@@ -6,11 +6,20 @@ const router = Router();
 router.get("/workouts", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const workouts = await getWorkouts();
-    res.render('pages/workouts.html', { workouts });
+    res.render("pages/workouts/index.html", { workouts });
     console.log(workouts);
   } catch (err) {
     next(err);
   }
+});
+
+router.get("/workouts/new", async (req: Request, res: Response, next: NextFunction) => {
+  res.render("pages/workouts/new.html");
+});
+
+router.post("/workouts/new", async(req: Request, res: Response, next: NextFunction) => {
+  console.log(req);
+  res.render("pages/workouts/new.html");
 })
 
 export default router;
--- app/services/workoutService.ts
+++ app/services/workoutService.ts
@@ -4,3 +4,8 @@ export async function getWorkouts() {
   const workouts = await prisma.workout.findMany();
   return workouts;  
 }
+
+export async function addWorkout(workout) {
+  const newWorkout = await prisma.workout.create({ data: workout })
+  return newWorkout;
+}
